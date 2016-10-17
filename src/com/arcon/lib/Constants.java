b32f4ebdce95c5ce2f2b3385ab4017cfefac0de8
LORDexile <exilexxxx@i.ua>
2016-10-18 01:02:28 +0300

    add constants to set program title & version

--- src/com/arcon/lib/Constants.java
+++ src/com/arcon/lib/Constants.java
@@ -3,5 +3,7 @@ package com.arcon.lib;
 public interface Constants {
     String adminLogin = "admin";
     String adminPassword = "root";
+    String PRODUCT_VERSION = "0.0.1";
+    String PROGRAM_TITLE = "TimeManager - ver:" + PRODUCT_VERSION;
 
 }
