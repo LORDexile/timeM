62922f9385d9db08e7215080070e8503ba7d7c74
LORDexile <exilexxxx@i.ua>
2016-10-18 01:05:52 +0300

    set controller + common constructor

--- src/com/arcon/ui/controller/MainFrameController.java
+++ src/com/arcon/ui/controller/MainFrameController.java
@@ -1,7 +1,29 @@
 package com.arcon.ui.controller;
 
-/**
- * Created by LORDexile on 18.10.2016.
- */
+import com.arcon.ui.view.MainFrame;
+
+import javax.swing.*;
+
 public class MainFrameController {
+
+    private MainFrame mainFrame;
+
+
+    private JTextField textFieldCardId;
+    private JToolBar toolMenuBar;
+    private JButton buttonOk;
+
+    public MainFrameController () {
+        initComponents();
+        initListeners();
+    }
+
+
+    private void initComponents() {
+
+    }
+
+    private void initListeners() {
+
+    }
 }
