4a94cebb0ccfea4392b70715f2827043cdeb53b4
LORDexile <exilexxxx@i.ua>
2016-10-18 01:05:12 +0300

    set constructor for main form

--- src/com/arcon/ui/view/MainFrame.java
+++ src/com/arcon/ui/view/MainFrame.java
@@ -1,7 +1,60 @@
 package com.arcon.ui.view;
 
-/**
- * Created by LORDexile on 18.10.2016.
- */
-public class MainFrame {
+import com.arcon.lib.Constants;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame implements Constants{
+
+    private JPanel mainPanel;
+
+    private JPanel panelLeft;
+    private JPanel panelRight;
+    private JPanel panelCenter;
+    private JPanel panelTop;
+    private JTextField textFieldCardId;
+    private JToolBar toolMenuBar;
+    private JButton buttonOk;
+
+    public MainFrame() {
+        setSize(1000, 500);
+        setContentPane(this.mainPanel);
+        setLocationRelativeTo(null);
+        setTitle(PROGRAM_TITLE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setResizable(true);
+        pack();
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public JPanel getPanelLeft() {
+        return panelLeft;
+    }
+
+    public JPanel getPanelRight() {
+        return panelRight;
+    }
+
+    public JPanel getPanelCenter() {
+        return panelCenter;
+    }
+
+    public JPanel getPanelTop() {
+        return panelTop;
+    }
+
+    public JTextField getTextFieldCardId() {
+        return textFieldCardId;
+    }
+
+    public JToolBar getToolMenuBar() {
+        return toolMenuBar;
+    }
+
+    public JButton getButtonOk() {
+        return buttonOk;
+    }
 }
