ebb19c5ff250d3eaeb64fd28d06f049e919b6cb7
LORDexile <exilexxxx@i.ua>
2016-10-18 18:23:44 +0300

    ver 0.0.5

--- src/com/arcon/lib/Constants.java
+++ src/com/arcon/lib/Constants.java
@@ -3,7 +3,7 @@ package com.arcon.lib;
 public interface Constants {
     String adminLogin = "admin";
     String adminPassword = "root";
-    String PRODUCT_VERSION = "0.0.4";
+    String PRODUCT_VERSION = "0.0.5";
     String PROGRAM_TITLE = "TimeManager - ver:" + PRODUCT_VERSION;
 
     double PRICE = 2000.0;
