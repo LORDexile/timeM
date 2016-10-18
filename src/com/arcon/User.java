4ca8128fe47c497892ec1b8be51018f294b7b6f7
LORDexile <exilexxxx@i.ua>
2016-10-18 17:08:49 +0300

    create enum UserType, to validate access

--- src/com/arcon/User.java
+++ src/com/arcon/User.java
@@ -1,7 +1,30 @@
 package com.arcon;
 
-/**
- * Created by LORDexile on 18.10.2016.
- */
 public class User {
+
+    private String userName;
+    private String password;
+    private UserType accessType;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+        accessType = UserType.USER;
+    }
+
+    public boolean isPasswordCorrect (String password) {
+        if (this.password.equals(password)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public UserType getAccessType() {
+        return accessType;
+    }
+
 }
--- src/com/arcon/UserType.java
+++ src/com/arcon/UserType.java
@@ -1,7 +1,7 @@
 package com.arcon;
 
-/**
- * Created by LORDexile on 18.10.2016.
- */
-public class UserType {
+public enum  UserType {
+    ADMIN,
+    MODERATOR,
+    USER;
 }
