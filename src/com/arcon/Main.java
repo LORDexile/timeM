57df629c2d245cd20bee283256c4564a84977315
LORDexile <exilexxxx@i.ua>
2016-10-28 22:14:55 +0300

    connect LogFrame

--- src/com/arcon/Main.java
+++ src/com/arcon/Main.java
@@ -1,15 +1,18 @@
 package com.arcon;
 
 import com.arcon.ui.controller.IOFrameController;
+import com.arcon.ui.controller.LogFrameController;
 import com.arcon.ui.controller.LoginFrameController;
 import com.arcon.ui.controller.MainFrameController;
 import com.arcon.ui.view.IOFrame;
+import com.arcon.ui.view.LogFrame;
 
 public class Main {
 
     public static MainFrameController mainFrameController;
     public static LoginFrameController loginFrameController;
     public static IOFrameController ioFrameController;
+    public static LogFrameController logFrameController;
 
     public static void main(String[] args) {
 
@@ -19,6 +22,8 @@ public class Main {
         mainFrameController = new MainFrameController();
 
         ioFrameController = new IOFrameController();
+
+        logFrameController = new LogFrameController();
 
     }
 }
