aefd52a348e82ce8422169f76aec38efa001da21
LORDexile <exilexxxx@i.ua>
2016-10-29 00:43:14 +0300

    fill transaction model

--- src/com/arcon/ui/model/TableTransactionsModel.java
+++ src/com/arcon/ui/model/TableTransactionsModel.java
@@ -1,7 +1,98 @@
 package com.arcon.ui.model;
 
-/**
- * Created by LORDexile on 28.10.2016.
- */
-public class TableTransactionsModel {
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class TableTransactionsModel implements TableModel{
+    List<Transaction> transactionList;
+
+    public TableTransactionsModel(List<Transaction> list) {
+        this.transactionList = list;
+    }
+
+    @Override
+    public int getRowCount() {
+        return transactionList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                return "Cash number";
+            case 1:
+                return "Action";
+            case 2:
+                return "User name";
+            case 3:
+                return "Date";
+            case 4:
+                return "Comment";
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                return int.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+            case 3:
+                return String.class;
+            case 4:
+                return String.class;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Transaction transaction = transactionList.get(rowIndex);
+        switch (columnIndex){
+            case 0:
+                return transaction.getCashNumber();
+            case 1:
+                return transaction.getActionType();
+            case 2:
+                return transaction.getUserName();
+            case 3:
+                SimpleDateFormat dateFormat = new SimpleDateFormat("'['HH:mm:ss']' d.MMM.yyyy", new Locale("ru"));
+                return dateFormat.format(transaction.getDate());
+            case 4:
+                return transaction.getComment();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
 }
