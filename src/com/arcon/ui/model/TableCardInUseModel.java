2851c1895dd8646defed8929887b22635928bbfd
LORDexile <exilexxxx@i.ua>
2016-10-28 23:07:14 +0300

    Create model of cardInUse table

--- src/com/arcon/ui/model/TableCardInUseModel.java
+++ src/com/arcon/ui/model/TableCardInUseModel.java
@@ -1,7 +1,131 @@
 package com.arcon.ui.model;
 
-/**
- * Created by LORDexile on 28.10.2016.
- */
-public class TableCardInUseModel {
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TableCardInUseModel implements TableModel {
+
+    private List<Card> cardList;
+    private Set<TableModelListener> listeners = new HashSet<>();
+
+    public TableCardInUseModel(List<Card> cardList) {
+        this.cardList = cardList;
+    }
+
+    @Override
+    public int getRowCount() {
+        return cardList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    /**
+     * Returns the name of the column at <code>columnIndex</code>.  This is used
+     * to initialize the table's column header name.  Note: this name does
+     * not need to be unique; two columns in a table can have the same name.
+     *
+     * @param columnIndex the index of the column
+     * @return the name of the column
+     */
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return "id";
+            case 1:
+                return "Date";
+        }
+        return "";
+    }
+
+    /**
+     * Returns the most specific superclass for all the cell values
+     * in the column.  This is used by the <code>JTable</code> to set up a
+     * default renderer and editor for the column.
+     *
+     * @param columnIndex the index of the column
+     * @return the common ancestor class of the object values in the model.
+     */
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return Long.class;
+            case 1:
+                return String.class;
+        }
+        return Object.class;
+    }
+
+    /**
+     * Returns true if the cell at <code>rowIndex</code> and
+     * <code>columnIndex</code>
+     * is editable.  Otherwise, <code>setValueAt</code> on the cell will not
+     * change the value of that cell.
+     *
+     * @param rowIndex    the row whose value to be queried
+     * @param columnIndex the column whose value to be queried
+     * @return true if the cell is editable
+     * @see #setValueAt
+     */
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Card card = cardList.get(rowIndex);
+        switch (columnIndex){
+            case 0:
+                return card.getId();
+            case 1:
+                SimpleDateFormat dateFormat = new SimpleDateFormat("'['HH:mm:ss']' d.MMM.yyyy", new Locale("ru"));
+
+                return dateFormat.format(card.getEnterTime());
+        }
+        return null;
+    }
+
+    /**
+     * Sets the value in the cell at <code>columnIndex</code> and
+     * <code>rowIndex</code> to <code>aValue</code>.
+     *
+     * @param aValue      the new value
+     * @param rowIndex    the row whose value is to be changed
+     * @param columnIndex the column whose value is to be changed
+     * @see #getValueAt
+     * @see #isCellEditable
+     */
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    /**
+     * Adds a listener to the list that is notified each time a change
+     * to the data model occurs.
+     *
+     * @param l the TableModelListener
+     */
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+        listeners.add(l);
+    }
+
+    /**
+     * Removes a listener from the list that is notified each time a
+     * change to the data model occurs.
+     *
+     * @param l the TableModelListener
+     */
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+        listeners.remove(l);
+    }
 }
