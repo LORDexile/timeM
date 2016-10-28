20d1a3986c4d74c73d0a060753c120cef634fd0d
LORDexile <exilexxxx@i.ua>
2016-10-28 20:42:26 +0300

    prepare controller and view

--- src/com/arcon/ui/controller/LogFrameController.java
+++ src/com/arcon/ui/controller/LogFrameController.java
@@ -1,7 +1,35 @@
 package com.arcon.ui.controller;
 
-/**
- * Created by LORDexile on 28.10.2016.
- */
+import com.arcon.ui.view.LogFrame;
+
+import javax.swing.*;
+
 public class LogFrameController {
+
+    private LogFrame logFrame;
+    private JButton button1;
+    private JTable tableCardInUse;
+    private JButton buttonRefresh;
+
+    public LogFrameController() {
+        initComponents();
+        initListeners();
+    }
+
+    public void showLogFrameWindow(){
+        logFrame.setVisible(true);
+    }
+
+    private void initComponents() {
+        logFrame = new LogFrame();
+
+        tableCardInUse = logFrame.getTableCardInUse();
+
+        buttonRefresh = logFrame.getButtonRefresh();
+    }
+
+    private void initListeners() {
+
+    }
+
 }
--- src/com/arcon/ui/view/LogFrame.java
+++ src/com/arcon/ui/view/LogFrame.java
@@ -1,7 +1,36 @@
 package com.arcon.ui.view;
 
-/**
- * Created by LORDexile on 28.10.2016.
- */
-public class LogFrame {
+import javax.swing.*;
+
+public class LogFrame extends JFrame{
+    private JTabbedPane tabbedPaneTables;
+    private JPanel panelLogMain;
+    private JPanel panelLogMainTop;
+    private JPanel panelTable;
+    private JButton button1;
+    private JTable tableCardInUse;
+    private JButton buttonRefresh;
+
+    public LogFrame() {
+        setSize(600,400);
+        setContentPane(this.panelLogMain);
+        setLocationRelativeTo(null);
+        setTitle("log:");
+        setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        setResizable(true);
+        pack();
+        setSize(600,400);
+    }
+
+    public JButton getButtonRefresh() {
+        return buttonRefresh;
+    }
+
+    public JTable getTableCardInUse() {
+        return tableCardInUse;
+    }
+
+    public JButton getButton1() {
+        return button1;
+    }
 }
