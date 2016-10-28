d009ef762f50a76a2b9a6a6c6e582cf20b195712
LORDexile <exilexxxx@i.ua>
2016-10-29 00:44:49 +0300

    refactore

--- src/com/arcon/ui/model/TableCardInUseModel.java
+++ src/com/arcon/ui/model/TableCardInUseModel.java
@@ -24,14 +24,6 @@ public class TableCardInUseModel implements TableModel {
         return 2;
     }
 
-    /**
-     * Returns the name of the column at <code>columnIndex</code>.  This is used
-     * to initialize the table's column header name.  Note: this name does
-     * not need to be unique; two columns in a table can have the same name.
-     *
-     * @param columnIndex the index of the column
-     * @return the name of the column
-     */
     @Override
     public String getColumnName(int columnIndex) {
         switch (columnIndex) {
@@ -43,14 +35,6 @@ public class TableCardInUseModel implements TableModel {
         return "";
     }
 
-    /**
-     * Returns the most specific superclass for all the cell values
-     * in the column.  This is used by the <code>JTable</code> to set up a
-     * default renderer and editor for the column.
-     *
-     * @param columnIndex the index of the column
-     * @return the common ancestor class of the object values in the model.
-     */
     @Override
     public Class<?> getColumnClass(int columnIndex) {
         switch (columnIndex) {
@@ -62,17 +46,6 @@ public class TableCardInUseModel implements TableModel {
         return Object.class;
     }
 
-    /**
-     * Returns true if the cell at <code>rowIndex</code> and
-     * <code>columnIndex</code>
-     * is editable.  Otherwise, <code>setValueAt</code> on the cell will not
-     * change the value of that cell.
-     *
-     * @param rowIndex    the row whose value to be queried
-     * @param columnIndex the column whose value to be queried
-     * @return true if the cell is editable
-     * @see #setValueAt
-     */
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return false;
@@ -92,38 +65,16 @@ public class TableCardInUseModel implements TableModel {
         return null;
     }
 
-    /**
-     * Sets the value in the cell at <code>columnIndex</code> and
-     * <code>rowIndex</code> to <code>aValue</code>.
-     *
-     * @param aValue      the new value
-     * @param rowIndex    the row whose value is to be changed
-     * @param columnIndex the column whose value is to be changed
-     * @see #getValueAt
-     * @see #isCellEditable
-     */
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
 
     }
 
-    /**
-     * Adds a listener to the list that is notified each time a change
-     * to the data model occurs.
-     *
-     * @param l the TableModelListener
-     */
     @Override
     public void addTableModelListener(TableModelListener l) {
         listeners.add(l);
     }
 
-    /**
-     * Removes a listener from the list that is notified each time a
-     * change to the data model occurs.
-     *
-     * @param l the TableModelListener
-     */
     @Override
     public void removeTableModelListener(TableModelListener l) {
         listeners.remove(l);
