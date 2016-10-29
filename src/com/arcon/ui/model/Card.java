dafd2c2fda8b13b3d966f5bf471357df50cb1b6d
LORDexile <exilexxxx@i.ua>
2016-10-29 16:59:53 +0300

    update card class,add new constructor to save card from DB, add getters, rework getTotalTime method, now he returns String like "1h 2m 3s"

--- src/com/arcon/ui/model/Card.java
+++ src/com/arcon/ui/model/Card.java
@@ -4,18 +4,33 @@ import com.arcon.lib.Constants;
 import java.util.Date;
 
 public class Card{
+    private int operationId;
     private long id;
+    private String userName;
+    private int price;
+    private double discount = 0;
     private Date enterTime;
     private Date exitTime;
+    private String enterTimeString;
+    private String exitTimeString;
     private long totalTime;
-    private double discount = 0;
+
+    public Card(int operationId, long id, String userName, int price, double discount, String enterTime, String exitTime) {
+        this.operationId = operationId;
+        this.id = id;
+        this.userName = userName;
+        this.price = price;
+        this.discount = discount;
+        enterTimeString = enterTime;
+        exitTimeString = exitTime;
+    }
 
     public Card(long id, Date date) {
         this.id = id;
         enterTime = date;
     }
 
-    public void setExitTime() {
+    public void setExitTime(){
         exitTime = new Date();
         //set time in seconds
         totalTime = (exitTime.getTime() - enterTime.getTime()) / 1000;
@@ -27,12 +42,29 @@ public class Card{
         }
     }
 
-    public long getTotalTime() {
-        return totalTime;
+    public int getOperationId() {
+        return operationId;
     }
 
     public long getId() {
         return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getPrice() {
+        if (discount != 0.0) {
+            price = (int) (totalTime * Constants.PRICE_SEC - (totalTime * Constants.PRICE_SEC * this.discount / 100));
+            return price;
+        }
+        price = (int) (totalTime * Constants.PRICE_SEC);
+        return  price;
+    }
+
+    public double getDiscount() {
+        return discount;
     }
 
     public Date getEnterTime() {
@@ -43,15 +75,30 @@ public class Card{
         return exitTime;
     }
 
-    public int getPrice() {
-        if (discount != 0.0) {
-            return (int) (totalTime * Constants.PRICE_SEC - (totalTime * Constants.PRICE_SEC * this.discount / 100));
-        }
-        return  (int) (totalTime * Constants.PRICE_SEC);
+    public String getEnterTimeString() {
+        return enterTimeString;
     }
 
-    public double getDiscount() {
-        return discount;
+    public String getExitTimeString() {
+        return exitTimeString;
     }
+
+    public String getTotalTime() {
+        long sek = totalTime;
+        int hour = 0;
+        int min = 0;
+        System.out.println("Время в сек: " + sek);
+        while (sek >= 60) {
+            sek -= 60;
+            min++;
+        }
+        while (min >= 60){
+            min -= 60;
+            hour++;
+        }
+
+        return hour + "h " + min + "m " + sek + "s";
+    }
+
 
 }
