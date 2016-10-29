338aefa2ad08995d2113aa1a53f04c377ce9e9b5
LORDexile <exilexxxx@i.ua>
2016-10-29 22:23:33 +0300

    Fill Discount model class

--- src/com/arcon/ui/model/TableDiscountsModel.java
+++ src/com/arcon/ui/model/TableDiscountsModel.java
@@ -1,7 +1,84 @@
 package com.arcon.ui.model;
 
-/**
- * Created by LORDexile on 29.10.2016.
- */
-public class TableDiscountsModel {
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.List;
+
+public class TableDiscountsModel implements TableModel{
+
+    List<Discount> discountList;
+
+    public TableDiscountsModel(List<Discount> discountList) {
+        this.discountList = discountList;
+    }
+
+    @Override
+    public int getRowCount() {
+        return discountList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                return "Discount";
+            case 1:
+                return "Description";
+            case 2:
+                return "User type";
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                return double.class;
+            case 1:
+                return String.class;
+            case 2:
+                return String.class;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Discount discount = discountList.get(rowIndex);
+        switch (columnIndex){
+            case 0:
+                return discount.getDiscount();
+            case 1:
+                return discount.getComment();
+            case 2:
+                return discount.getUserType();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
 }
