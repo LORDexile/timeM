8d0e12064a319ba8d59d7fdbfbf03853c0fd02d5
LORDexile <exilexxxx@i.ua>
2016-10-29 22:58:16 +0300

    add method that convert String userType to UserType class

--- src/com/arcon/ui/model/UserType.java
+++ src/com/arcon/ui/model/UserType.java
@@ -3,5 +3,17 @@ package com.arcon.ui.model;
 public enum  UserType {
     ADMIN,
     MODERATOR,
-    USER;
+    USER,
+    INDEFINED;
+
+    public static UserType toUserType(String userType){
+        if (userType.equals("ADMIN")){
+            return ADMIN;
+        } else if (userType.equals("MODERATOR")){
+            return MODERATOR;
+        } else if (userType.equals("USER")){
+            return USER;
+        }
+        return INDEFINED;
+    }
 }
