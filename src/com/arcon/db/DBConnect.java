8d5b34b11680391aa9687c3a22468cd0d0eada63
LORDexile <exilexxxx@i.ua>
2016-10-29 22:22:07 +0300

    Add to method that returns DiscountList, User Type parameter

--- src/com/arcon/db/DBConnect.java
+++ src/com/arcon/db/DBConnect.java
@@ -326,8 +326,8 @@ public class DBConnect{
         try {
             resSet = statement.executeQuery("SELECT * FROM Discount");
             while (resSet.next()) {
-                if (resSet.getString("UserType").equals(Constants.getUserType()) || Constants.getUserType().equals(UserType.ADMIN)) {
-                    list.add(new Discount(resSet.getDouble("Discount"), resSet.getString("Comment")));
+                if (resSet.getString("UserType").equals(Constants.getUserType()) || Constants.getUserType().equals(UserType.ADMIN.toString())) {
+                    list.add(new Discount(resSet.getDouble("Discount"), resSet.getString("Comment"), resSet.getString("UserType")));
                 }
             }
         }catch (SQLException e) {
