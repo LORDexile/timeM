b5020516794e307c238d8fac464816173bb9b7a1
LORDexile <exilexxxx@i.ua>
2016-10-29 23:37:27 +0300

    fill users TableModel

--- src/com/arcon/ui/model/TableUsersModel.java
+++ src/com/arcon/ui/model/TableUsersModel.java
@@ -1,7 +1,82 @@
 package com.arcon.ui.model;
 
-/**
- * Created by LORDexile on 29.10.2016.
- */
-public class TableUsersModel {
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.List;
+
+public class TableUsersModel implements TableModel{
+
+    private List<User> userList;
+
+    public TableUsersModel(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public int getRowCount() {
+        return userList.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return "Name";
+            case 1:
+                return "Type";
+            case 2:
+                return "Comment";
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+            case 1:
+            case 2:
+                return String.class;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        User user = userList.get(rowIndex);
+        switch (columnIndex) {
+            case 0:
+                return user.getUserName();
+            case 1:
+                return user.getUserType();
+            case 2:
+                return user.getComment();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
 }
