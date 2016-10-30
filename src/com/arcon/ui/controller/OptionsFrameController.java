6c28b84f7efe1ac60dbdb53863e65de1a625e257
LORDexile <exilexxxx@i.ua>
2016-10-30 20:03:44 +0200

    add listener to Show selected context pane

--- src/com/arcon/ui/controller/OptionsFrameController.java
+++ src/com/arcon/ui/controller/OptionsFrameController.java
@@ -1,9 +1,12 @@
 package com.arcon.ui.controller;
 
+import com.arcon.Main;
 import com.arcon.lib.Constants;
 import com.arcon.ui.view.OptionsFrame;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OptionsFrameController {
     OptionsFrame optionsFrame;
@@ -12,11 +15,14 @@ public class OptionsFrameController {
     private JButton buttonMenuPrice;
     private JButton buttonMenuDiscounts;
     private JButton buttonMenuUsers;
+    private JButton buttonMenuProgram;
+
+    private JPanel panelContextChangeUser;
 
     private JLabel labelUserName;
     private JLabel labelUserType;
     private JButton buttonChangeUser;
-    private JButton buttonMenuProgram;
+
 
     public OptionsFrameController() {
         initComponents();
@@ -26,15 +32,19 @@ public class OptionsFrameController {
     private void initComponents() {
         optionsFrame = new OptionsFrame();
 
+        panelContextChangeUser = optionsFrame.getPanelContextChangeUser();
+
         buttonMenuChangeUser = optionsFrame.getButtonMenuChangeUser();
         labelUserName = optionsFrame.getLabelUserName();
         labelUserType = optionsFrame.getLabelUserType();
         buttonChangeUser = optionsFrame.getButtonChangeUser();
-        buttonMenuProgram = optionsFrame.getButtonMenuProgram();
     }
 
     private void initListeners() {
 
+        buttonMenuChangeUser.addActionListener(new buttonMenuChangeUserActionListener());
+
+        buttonChangeUser.addActionListener(new buttonChangeUserActionListener());
     }
 
     public void showOptionsFrameWindow() {
@@ -45,5 +55,36 @@ public class OptionsFrameController {
     private void setCurrentUserInfo(){
         labelUserName.setText("<html><font color='red'>" + Constants.getUserName() + "</font></html>");
         labelUserType.setText("<html><font color='red'>" + Constants.getUserType() + "</font></html>");
+    }
+
+    private void setContextPanel(int number){
+        hideContextPanels();
+        switch (number){
+            case 1:
+                panelContextChangeUser.setVisible(true);
+                break;
+            case 2:
+        }
+    }
+
+    private void hideContextPanels(){
+        panelContextChangeUser.setVisible(false);
+    }
+
+    private class buttonChangeUserActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Main.loginFrameController.showLoginFrameWindow();
+            optionsFrame.setVisible(false);
+        }
+    }
+
+    private class buttonMenuChangeUserActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setContextPanel(1);
+        }
     }
 }
