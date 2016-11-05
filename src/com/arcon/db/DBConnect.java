2c67e1578009f9829d539af03f61dbd190f4a2e1
LORDexile <exilexxxx@i.ua>
2016-11-05 17:58:35 +0200

    now shows only active discounts

--- src/com/arcon/db/DBConnect.java
+++ src/com/arcon/db/DBConnect.java
@@ -397,7 +397,7 @@ public class DBConnect{
         try {
             resSet = statement.executeQuery("SELECT * FROM Discount");
             while (resSet.next()) {
-                if (resSet.getString("UserType").equals(Constants.getUserType()) || Constants.getUserType().equals(UserType.ADMIN.toString())) {
+                if ((resSet.getString("UserType").equals(Constants.getUserType()) || Constants.getUserType().equals(UserType.ADMIN.toString())) && resSet.getInt("Active")==1) {
                     list.add(new Discount(resSet.getDouble("Discount"), resSet.getString("Comment"), resSet.getString("UserType"), resSet.getInt("Active")));
                 }
             }
